570ada6c2ff6c7396b604b2bb48a242bd546df6b
samitkumarpatel <samitkumarpatel@gmail.com>
2020-01-30 04:43:45 +0100

    Initial Commit

---
+++ src/main/java/net/samitkumar/math/MathUtils.java
@@ -0,0 +1,9 @@
+package net.samitkumar.math;
+
+public class MathUtils {
+	
+	public int add(int a,int b) {
+		return a + b;
+	}
+
+}
---
+++ src/test/java/net/samitkumar/math/MathUtilsTest.java
@@ -0,0 +1,14 @@
+package net.samitkumar.math;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
