8a120e8b8f23b41396986ec4fac14baba5390c54
samitkumarpatel <samitkumarpatel@gmail.com>
2020-01-30 05:10:22 +0100

    Update one more test

--- src/test/java/net/samitkumar/math/MathUtilsTest.java
+++ src/test/java/net/samitkumar/math/MathUtilsTest.java
@@ -7,8 +7,16 @@ import org.junit.jupiter.api.Test;
 class MathUtilsTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testAdd() {
+		MathUtils m = new MathUtils();
+		int val = m.add(10, 1);
+		assertEquals(11, val,"Should be 11");
 	}
-
+	
+	
+	@Test
+	void testComputeRadius() {
+		MathUtils mathUtils = new MathUtils();
+		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10),"Should return right circle area");
+	}
 }
